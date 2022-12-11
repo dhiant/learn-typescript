e7306bbaf3b5860e331fa0c12eef69ee2ab48f0d
Sushant Dhimal <sushant07.dhimal@gmail.com>
2022-12-11 21:55:47 +0545

    day2: object and union types along with type aliases

--- app.js
+++ app.js
@@ -31,3 +31,33 @@ names.forEach(function (s) {
 names.forEach(function (s) {
     // console.log(s.toUppercase());
 });
+// object types
+// 1. passing an point-like object to a function
+function getCarModel(latest) {
+    console.log("the latest model of lamborghini is ".concat(latest.lamborghini, " and bugati is ").concat(latest.bugati));
+}
+getCarModel({
+    lamborghini: "Aventador LP 780-4",
+    bugati: "CHIRON Super Sport"
+});
+// 2. using the question mark(?) symbol we can specify the some or all of their properties are optional
+function getEVCarModel(latest) {
+    var _a;
+    console.log("the latest Tesla model S is ".concat(latest.model_S));
+    // use optional chaining operator to check if property exists in object
+    console.log("the latest Tesla model S is ".concat(latest.model_S, " and Volvo is ").concat((_a = latest.Volvo) === null || _a === void 0 ? void 0 : _a.toLocaleLowerCase()));
+}
+getEVCarModel({ model_S: "Model S Plaid" });
+getEVCarModel({ model_S: "Model S Plaid", Volvo: "Volvo XC40" });
+// ### Union types >>> build new types using existing ones using a variety of operators
+function printId(id) {
+    console.log("".concat(id.toLocaleString()));
+    console.log("id can be either of string or number and value is ".concat(id));
+}
+// both are valid
+printId(111);
+printId("222");
+function getArea(a) {
+    return a.x * a.y;
+}
+getArea({ x: 4, y: 5 });
--- app.ts
+++ app.ts
@@ -42,3 +42,53 @@ names.forEach(function (s) {
 names.forEach((s) => {
   // console.log(s.toUppercase());
 });
+
+// object types
+// 1. passing an point-like object to a function
+function getCarModel(latest: { lamborghini: string; bugati: string }) {
+  console.log(
+    `the latest model of lamborghini is ${latest.lamborghini} and bugati is ${latest.bugati}`
+  );
+}
+
+getCarModel({
+  lamborghini: "Aventador LP 780-4",
+  bugati: "CHIRON Super Sport",
+});
+
+// 2. using the question mark(?) symbol we can specify the some or all of their properties are optional
+function getEVCarModel(latest: { model_S: string; Volvo?: string }) {
+  console.log(`the latest Tesla model S is ${latest.model_S}`);
+
+  // use optional chaining operator to check if property exists in object
+  console.log(
+    `the latest Tesla model S is ${
+      latest.model_S
+    } and Volvo is ${latest.Volvo?.toLocaleLowerCase()}`
+  );
+}
+getEVCarModel({ model_S: "Model S Plaid" });
+getEVCarModel({ model_S: "Model S Plaid", Volvo: "Volvo XC40" });
+
+// ### Union types >>> build new types using existing ones using a variety of operators
+function printId(id: string | number) {
+  console.log(`${id.toLocaleString()}`);
+  console.log(`id can be either of string or number and value is ${id}`);
+}
+
+// both are valid
+printId(111);
+printId("222");
+
+// while working with union types remember the method we use must be available for both of the types
+
+// ### Type Aliases >>> create an alias for any type
+type Area = {
+  x: number;
+  y: number;
+};
+
+function getArea(a: Area) {
+  return a.x * a.y;
+}
+getArea({ x: 4, y: 5 });
